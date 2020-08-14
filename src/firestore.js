0e9f92b48cb3b973574d8ce53770af9ca1867eaa
Shafin Hasnat <shafinhasnat97@gmail.com>
2020-08-14 16:43:54 +0600

    crd-done

--- src/firestore.js
+++ src/firestore.js
@@ -1,13 +1,13 @@
 import * as firebase from "firebase/app";
 import "firebase/firestore";
 const config = {
-  apiKey: "AIzaSyCRem4OVGzZT_Uwu995kLjm8q4TSI1-FNg",
-  authDomain: "hale-monument-250206.firebaseapp.com",
-  databaseURL: "https://hale-monument-250206.firebaseio.com",
-  projectId: "hale-monument-250206",
-  storageBucket: "hale-monument-250206.appspot.com",
-  messagingSenderId: "698105936156",
-  appId: "1:698105936156:web:08ad6fc77025abae93a2d9",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 firebase.initializeApp(config);
 
